a0928fe6e8f61501580415f8ee1f6149ca45decf
my file <vagifrg@code.edu.az>
2022-08-11 18:38:06 +0400

    new file

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            User n = new User();
+            Console.WriteLine(n.Posword);
+            Console.WriteLine(n.Username);
+        }
+    }
+}
---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class User
+    {
+        private string username;
+        private string posword;
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                if (value.Length > 6)
+                {
+                    username = value;
+                }
+            }
+            
+        }
+        public string Posword
+        {
+            get
+            {
+                return posword;
+            }
+            set
+            {
+                if (value.Length > 8)
+                {
+                    value = posword;
+                }
+            }
+        }
+    }
+}
